8df162b21cf57b89526384c2050c9881cc849943
root <root@fooww.com>
2020-12-13 16:54:56 +0800

    Initial Commit

---
+++ MvcExplain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MvcExplain
+//
+//  Created by luxury on 2020/12/13.
+//  Copyright © 2020 luxury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MvcExplainTests/MvcExplainTests.swift
@@ -0,0 +1,34 @@
+//
+//  MvcExplainTests.swift
+//  MvcExplainTests
+//
+//  Created by luxury on 2020/12/13.
+//  Copyright © 2020 luxury. All rights reserved.
+//
+
+import XCTest
+@testable import MvcExplain
+
+class MvcExplainTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
